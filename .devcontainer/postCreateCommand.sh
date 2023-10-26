6791ff7eeb11cc0b637a5e534179c4017882b7c8
Jules <renaud.jules@gmail.com>
2023-10-26 22:36:09 +0000

    wip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,13 +4,11 @@ REPO_FOLDER="/workspaces/$RepositoryName"
 # Apache
 sudo chmod 777 /etc/apache2/sites-available/000-default.conf
 sudo sed "s@.*DocumentRoot.*@\tDocumentRoot $PWD/wordpress@" .devcontainer/000-default.conf > /etc/apache2/sites-available/000-default.conf
-update-rc.d apache2 defaults 
+update-rc.d apache2 defaults
 service apache2 start
 
-LOCALE="de_DE"
-
 # WordPress Core install
-wp core download --locale=$LOCALE --path=wordpress
+wp core download --path=wordpress
 cd wordpress
 wp config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db
 LINE_NUMBER=`grep -n -o 'stop editing!' wp-config.php | cut -d ':' -f 1`
@@ -19,21 +17,14 @@ wp core install --url=https://$(CODESPACE_NAME) --title=WordPress --admin_user=a
 
 # Selected plugins
 wp plugin delete akismet
-wp plugin install show-current-template --activate
-wp plugin activate wp-codespace
-
-# Demo content for WordPress
-wp plugin install wordpress-importer --activate
-curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml > demo-content.xml
-wp import demo-content.xml --authors=create
-rm demo-content.xml
+wp plugin delete hello
 
 #Xdebug
 echo xdebug.log_level=0 | sudo tee -a /usr/local/etc/php/conf.d/xdebug.ini
 
 # install dependencies
 cd $REPO_FOLDER
-npm install 
+npm install
 composer install
 
 # Setup local plugin
